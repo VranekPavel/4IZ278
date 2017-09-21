1612cb8c153d81e483659aedd78b8cf6b8d1042e
VranekPavel <vranek.pavel95@gmail.com>
2017-09-21 13:43:09 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 //pripojeni do db na serveru eso.vse.cz
-$db = new PDO('mysql:host=127.0.0.1;dbname=vrap00;charset=utf8', 'vrap00', 'BJGJQII4qZtvCtX0dA');
+$db = new PDO('mysql:host=127.0.0.1;dbname=vrap00;charset=utf8', 'vrap00', '******************');
 
 //vyhazuje vyjimky v pripade neplatneho SQL vyrazu
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION)
